65e410e90515d2640c154a7d0248566c438d7467
remigarcia31 <remi@ordinatlederemi.home>
2024-10-26 10:43:25 +0200

    add answers files and check the solution

--- app.py
+++ app.py
@@ -1,90 +1,91 @@
 # pylint: disable=missing-module-docstring
 
-import io
-
+import os
+import logging
 import duckdb
-import pandas as pd
 import streamlit as st
 
-CSV1 = """
-    beverage,price
-    orange juice,2.5
-    Expresso,2
-    Tea,3
-"""
-beverages = pd.read_csv(io.StringIO(CSV1))
 
-CSV2 = """
-    food_item,food_price
-    cookie,2.5
-    chocolatine,2
-    muffin,3
-"""
-food_items = pd.read_csv(io.StringIO(CSV2))
+if "data" not in os.listdir():
+    print("creating folder data")
+    logging.error(os.listdir())
+    logging.error("creating folder data")
+    os.mkdir("data")
 
-ANSWER_SQL = """
-    SELECT * FROM beverages
-    CROSS JOIN food_items
-"""
-solution_df = duckdb.sql(ANSWER_SQL).df()
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    exec(open("init_db.py").read())
+    # subprocess.run(["python", "init_db.py"])
 
-st.write("Spaced Repetition System SQL practice")
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
+
+
+def check_users_solution(user_query: str) -> None:
+    """
+    Checks that user SQL query is correct by:
+    1: checking the columns
+    2: checking the values
+    :param user_query: a string containing the query inserted by the user
+    """
+    result = con.execute(user_query).df()
+    st.dataframe(result)
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
+    except KeyError as e:
+        st.write("Some columns are missing")
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+        )
+
+
 with st.sidebar:
-    option = st.selectbox(
-        "What would you like to review ?",
-        ("Join", "Group By", "Windows Functions"),
+    available_themes_df = con.execute("SELECT DISTINCT theme FROM memory_state").df()
+    theme = st.selectbox(
+        "What would you like to review?",
+        available_themes_df["theme"].unique(),
         index=None,
         placeholder="Select a theme...",
     )
-    st.write("You selected:", option)
+    if theme:
+        st.write(f"You selected {theme}")
+        select_exercise_query = f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+    else:
+        select_exercise_query = f"SELECT * FROM memory_state"
+
+    exercise = (
+        con.execute(select_exercise_query)
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
+    st.write(exercise)
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
-query = st.text_area(label="Votre code SQL ici", key="user _input")
+form = st.form("my_form")
+query = form.text_area(label="votre code SQL ici", key="user_input")
+form.form_submit_button("Submit")
+
 if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
+    check_users_solution(query)
 
-    if len(result.columns) != len(
-        solution_df.columns
-    ):  # replace with try result = result[solution.columns]
-        st.write("Some columns are missing")
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"result has a {n_lines_difference} lines difference with the solution"
-        )
-
-    if len(result.columns) != len(
-        solution_df.columns
-    ):  # replace with try result = result[solution.columns]
-        st.write("Some columns are missing")
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("Some columns are missing")
-    n_lines_difference = result.shape[0] - solution_df.shape[0]
-    if n_lines_difference != 0:
-        st.write(
-            f"result has a {n_lines_difference} lines difference with the solution"
-        )
 
 tab2, tab3 = st.tabs(["Tables", "Solution"])
-
 with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution_df)
+    exercise_tables = exercise.loc[0, "tables"]
+    for table in exercise_tables:
+        st.write(f"table: {table}")
+        df_table = con.execute(f"SELECT * FROM {table}").df()
+        st.dataframe(df_table)
 
 with tab3:
-    st.write(ANSWER_SQL)
+    st.write(answer)
